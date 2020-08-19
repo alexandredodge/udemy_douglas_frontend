e6f9d5d30706eb801d91286fb2d75011acc37914
unknown <alexandredodge@hotmail.com>
2020-08-19 20:45:52 -0300

    alteracoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TemplateModule } from './template/template.module'
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
+
+import { ClientesModule } from './clientes/clientes.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    TemplateModule
+    TemplateModule,
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
