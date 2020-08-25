fc515d23e876f1a1abd037c01038e89093744588
Jose Alexandre Oliveira de Souza <jose.osouza@PMCG.IMTI>
2020-08-25 17:05:10 -0400

    alteracoes

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router, ActivatedRoute, Params } from '@angular/router'
 import { Cliente } from '../cliente'
 import { ClientesService } from '../../clientes.service'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-clientes-form',
@@ -13,26 +14,50 @@ export class ClientesFormComponent implements OnInit {
   cliente: Cliente; 
   success: boolean = false;
   errors: String[];
+  id: number;
 
   constructor( 
-    private service : ClientesService, 
-    private router:Router) { 
+      private service : ClientesService, 
+      private router:Router,
+      private activatedRoute: ActivatedRoute
+      ) { 
     this.cliente = new Cliente();
   }
 
   ngOnInit(): void {
+    let parametro : Observable<Params> = this.activatedRoute.params
+    parametro.subscribe( urlParams => {
+        this.id = urlParams['id'];
+        this.service
+          .getClientesById( this.id )
+          .subscribe(
+              response => this.cliente = response,
+              errorResponse => this.cliente = new Cliente()
+          )
+    } )
   }
 
   onSubmit(){
-    this.service.salvar(this.cliente).subscribe( response =>{
-      this.success = true;
-      this.errors = null;
-      this.cliente = response;
-    }, errorResponse => {
-      this.success = false;
-      this.errors = errorResponse.error.errors;
-    }
-    )
+        this.service.atualizar(this.cliente)
+        .subscribe(response => {
+          this.success = true;
+            this.errors = null;
+        }, errorResponse => {
+          this.errors = ['Erro ao atualizar o cliente.']
+        } )
+    if(this.id){
+
+    }else{
+        this.service.salvar(this.cliente).subscribe( response =>{
+          this.success = true;
+          this.errors = null;
+          this.cliente = response;
+        }, errorResponse => {
+          this.success = false;
+          this.errors = errorResponse.error.errors;
+        }
+        )
+      }
   }
 
   voltar(){
